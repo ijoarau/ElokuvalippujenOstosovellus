be07e4513ea7d11fb0f462e6d75c8402cc14eb00
pinja.malkki <pinja.malkki@LAPTOP-395SAC3J>
2025-09-10 12:24:49 +0300

    Add project files.

---
+++ ElokuvalippujenOstosovellus/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElokuvalippujenOstosovellus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
